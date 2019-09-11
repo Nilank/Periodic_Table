f080289f2a6504db543737760951624d1d7c31f3
Nilank <sharmanilank.com>
2019-09-10 23:38:19 -0400

    Updates

---
+++ precache-manifest.2eb1e308c42eb05b12674f28a96ca771.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f8d7f2b2464e884d3edc302993aa5d",
+    "url": "/Periodic_Table/index.html"
+  },
+  {
+    "revision": "81b9b8802332716a5fc5",
+    "url": "/Periodic_Table/static/css/main.0219eea1.chunk.css"
+  },
+  {
+    "revision": "d0ed22c19d732e5b72f1",
+    "url": "/Periodic_Table/static/js/2.1a7eca3a.chunk.js"
+  },
+  {
+    "revision": "81b9b8802332716a5fc5",
+    "url": "/Periodic_Table/static/js/main.dfda85f7.chunk.js"
+  },
+  {
+    "revision": "fc48cfc7377af4932d50",
+    "url": "/Periodic_Table/static/js/runtime~main.a97b9717.js"
+  }
+]);
